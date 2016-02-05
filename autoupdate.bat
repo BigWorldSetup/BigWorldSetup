54409a47c5264fffcec3298d62725b5c82b323d6
ALIENQuake <alienquake@hotmail.com>
2016-02-05 14:36:47 +0100

    use internal git

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 echo Checking for BWS updates...
-git remote update
-FOR /F "delims=" %%A in ('git rev-parse @{0}') DO SET _remote=%%A
-FOR /F "delims=" %%A in ('git rev-parse HEAD') DO SET _locals=%%A
+"BiG World Setup\Tools\Git\bin\git.exe" remote update
+FOR /F "delims=" %%A in ('"BiG World Setup\Tools\Git\bin\git.exe" rev-parse @{0}') DO SET _remote=%%A
+FOR /F "delims=" %%A in ('"BiG World Setup\Tools\Git\bin\git.exe" rev-parse HEAD') DO SET _locals=%%A
 
 IF NOT %_locals% == %_remote% (
   echo Saving any local changes...
-  git stash
+  "BiG World Setup\Tools\Git\bin\git.exe" stash
   echo Downloading BWS updates...
-  git pull --rebase
+  "BiG World Setup\Tools\Git\bin\git.exe" pull --rebase
   echo Merging any local changes...
-  git stash pop
+  "BiG World Setup\Tools\Git\bin\git.exe" stash pop
   echo Done with BWS auto-update!
   echo If you see a MERGE WARNING above, resolve it if you know how or else use the full update
   pause
